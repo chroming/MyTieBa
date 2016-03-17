f4fb82396d2d59f688c8d064943edbad1ec7d10d
chroming <guangzhishang@gmail.com>
2016-03-17 16:31:15 +0800

    根据PEP8修改部分格式

--- MyTieBa.py
+++ MyTieBa.py
@@ -6,9 +6,9 @@ import re
 
 Header = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:44.0) Gecko/20100101 Firefox/44.0',
-        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Language':'en-US,en;q=0.5',
-        'Accept-Encoding':'gzip, deflate'
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate'
 }
 
 di = u'第'
@@ -17,9 +17,9 @@ shuaxin = u'刷新'
 hui = u'回'
 stopnum = 3
 
+
 # 贴吧类
 class TieBaInfo(object):
-
     def __init__(self,URL):
         self.HeadURL = URL
 
@@ -37,33 +37,32 @@ class TieBaInfo(object):
         self.TieziListAll = re.findall(r'class\=\"i.{0,3}\"\>\<a.href\=\"(.{70,90}kz=\d+)\&.*?\>\d*\.\&\#160\;(.*?)\<\/a\>.*?%s(\d*)'%hui,GetPageList.text)
 
 
-
 # 帖子信息类
 class TieziInfo(object):
-    def __init__(self,TieziUrl):
+    def __init__(self, TieziUrl):
         self.TieziUrl = TieziUrl
 
     # 获取帖子标题,时间,回复页数
     def GetTieziInfoFun(self):
-        GetTieziText = requests.get(self.TieziUrl,headers = Header).text
-        self.Tiezititle = re.findall(r'\<\/style\>\<title\>(.*?)\<\/title\>',GetTieziText)[0]
+        GetTieziText = requests.get(self.TieziUrl, headers=Header).text
+        self.Tiezititle = re.findall(r'</style><title>(.*?)</title>', GetTieziText)[0]
         try:
-            self.TieziTime = re.findall(r'div class\=\"i\"\>1%s\..*?\<span class\=\"b\"\>(.*?)(\d{1,2}\:\d{1,2})\<\/span\>'%lou,GetTieziText)[0]
+            self.TieziTime = re.findall(r'div class\=\"i\">1%s\..*?<span class\=\"b\">(.*?)(\d{1,2}\:\d{1,2})</span>' % lou, GetTieziText)[0]
         except:
             self.TieziTime = (u'0', u'0')
 
         # 帖子回复页数
         try:
-            self.TieziPageNumber = int(re.findall(r'%s1\/(\d*)'%di,GetTieziText)[0])
+            self.TieziPageNumber = int(re.findall(r'%s1/(\d*)' % di, GetTieziText)[0])
         except:
             self.TieziPageNumber = 1
 
 
         self.TieziZhuti = re.findall(r'div class\=\"i\"\>1%s\. (.*?)\<br\/\>\<br\/\>\<span class\=\"g\"\>'%lou,GetTieziText)
 
-    def GetTieziHuifulist(self,Huililist):
+    def GetTieziHuifulist(self, Huililist):
 
-        GetTieziTextAll = requests.get(Huililist,headers = Header).text
+        GetTieziTextAll = requests.get(Huililist, headers = Header).text
         self.TieziHuifuText = re.findall(r'%s\<\/a\>\<\/div\>\<div\ class\=\"d\"\>(.*?)form action\=\"submit\"'%shuaxin,GetTieziTextAll,re.S)[0]
         # 以下正则组匹配内容依次为:帖子楼层;帖子内容;回帖ID;回帖时间;回帖链接
         self.TieziHuifu = re.findall(r'div class\=\"i\"\>(\d*)%s\. (.*?)\<br\/\>\<span class\=\"g\".*?href\=\".*?\"\>(.*?)\<\/a.*?span class\=\"b\"\>(\d.*?\d)\<.*?\<a href\=\"(flr.*?)\"'%lou, self.TieziHuifuText)
@@ -81,7 +80,7 @@ print tieba.PageNumber
 HeadUrl = tieba.HeadURL
 
 # 循环获取前stopnum页帖子URL
-for LN in range(0,stopnum*20,20):
+for LN in range(0, stopnum*20, 20):
     print "***************************************************************************"
     PageListUrl = 'http://tieba.baidu.com/mo/m?kw=%s&lm=&pn='%HeadUrl+str(LN)
     tieba.GetPageListFun(PageListUrl)
@@ -89,7 +88,7 @@ for LN in range(0,stopnum*20,20):
     # 循环帖子URL获取HTML
     for tz in tieba.TieziListAll:
         TieziUrl = 'http://tieba.baidu.com'+tz[0]
-        print tz[0],tz[1],tz[2]
+        print tz[0], tz[1], tz[2]
         # print TieziUrl
         tiezi = TieziInfo(TieziUrl)
         tiezi.GetTieziInfoFun()
@@ -97,8 +96,8 @@ for LN in range(0,stopnum*20,20):
         print tiezi.TieziTime
         print('------------------------')
         if tiezi.TieziTime[0] is not None:
-            if re.match(r'3\-\d{1,2}',tiezi.TieziTime[0]):
-                for TZi in range(0,tiezi.TieziPageNumber*10,10):
+            if re.match(r'3\-\d{1,2}', tiezi.TieziTime[0]):
+                for TZi in range(0, tiezi.TieziPageNumber*10, 10):
 
                     TiezilistUrl = TieziUrl+'&pn=%s'%TZi
                     tiezi.GetTieziHuifulist(TiezilistUrl)
