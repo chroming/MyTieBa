133d9d09a3b795db0237a8346ab202fff7175c15
chroming <guangzhishang@gmail.com>
2016-03-17 16:18:39 +0800

    获取回帖实际内容
    通过删除<>标签获得实际回复的内容。并计算回帖字数。

--- MyTieBa.py
+++ MyTieBa.py
@@ -17,13 +17,13 @@ shuaxin = u'刷新'
 hui = u'回'
 stopnum = 3
 
-#贴吧类
+# 贴吧类
 class TieBaInfo(object):
 
     def __init__(self,URL):
         self.HeadURL = URL
 
-    #获取首页HTML及本吧帖子总页数
+    # 获取首页HTML及本吧帖子总页数
     def GetFirstPageFun(self):
 
         FirstPageUrl = 'http://tieba.baidu.com/mo/m?kw=%s&lm=&pn=0'%self.HeadURL
@@ -31,19 +31,19 @@ class TieBaInfo(object):
         GetFirstPage.encoding = 'utf-8'
         self.PageNumber = re.findall(r'%s1\/(\d*)'%di,GetFirstPage.text)[0]
 
-    #获取前stopnum页数的帖子URL,主题,总回复数
+    # 获取前stopnum页数的帖子URL,主题,总回复数
     def GetPageListFun(self,Allurl):
         GetPageList = requests.get(Allurl,headers = Header)
         self.TieziListAll = re.findall(r'class\=\"i.{0,3}\"\>\<a.href\=\"(.{70,90}kz=\d+)\&.*?\>\d*\.\&\#160\;(.*?)\<\/a\>.*?%s(\d*)'%hui,GetPageList.text)
 
 
 
-#帖子信息类
+# 帖子信息类
 class TieziInfo(object):
     def __init__(self,TieziUrl):
         self.TieziUrl = TieziUrl
 
-    #获取帖子标题,时间,回复页数
+    # 获取帖子标题,时间,回复页数
     def GetTieziInfoFun(self):
         GetTieziText = requests.get(self.TieziUrl,headers = Header).text
         self.Tiezititle = re.findall(r'\<\/style\>\<title\>(.*?)\<\/title\>',GetTieziText)[0]
@@ -52,7 +52,7 @@ class TieziInfo(object):
         except:
             self.TieziTime = (u'0', u'0')
 
-        #帖子回复页数
+        # 帖子回复页数
         try:
             self.TieziPageNumber = int(re.findall(r'%s1\/(\d*)'%di,GetTieziText)[0])
         except:
@@ -70,22 +70,27 @@ class TieziInfo(object):
 
 
 
+def GetRealContent(content):
+    realcontent = re.sub(r'<.*?>', '', content)
+    return realcontent
+
+
 tieba = TieBaInfo('%E5%8D%97%E4%BA%AC%E7%90%86%E5%B7%A5%E5%A4%A7%E5%AD%A6')
 tieba.GetFirstPageFun()
 print tieba.PageNumber
 HeadUrl = tieba.HeadURL
 
-#循环获取前stopnum页帖子URL
+# 循环获取前stopnum页帖子URL
 for LN in range(0,stopnum*20,20):
     print "***************************************************************************"
     PageListUrl = 'http://tieba.baidu.com/mo/m?kw=%s&lm=&pn='%HeadUrl+str(LN)
     tieba.GetPageListFun(PageListUrl)
 
-    #循环帖子URL获取HTML
+    # 循环帖子URL获取HTML
     for tz in tieba.TieziListAll:
         TieziUrl = 'http://tieba.baidu.com'+tz[0]
         print tz[0],tz[1],tz[2]
-        #print TieziUrl
+        # print TieziUrl
         tiezi = TieziInfo(TieziUrl)
         tiezi.GetTieziInfoFun()
         print tiezi.TieziPageNumber
@@ -97,10 +102,12 @@ for LN in range(0,stopnum*20,20):
 
                     TiezilistUrl = TieziUrl+'&pn=%s'%TZi
                     tiezi.GetTieziHuifulist(TiezilistUrl)
-                    #print tiezi.TieziHuifu
+                    # print tiezi.TieziHuifu
                     for hf in tiezi.TieziHuifu:
+                        realcontent = GetRealContent(hf[1])
                         print hf[0]
-                        print hf[1]
+                        print realcontent
+                        print len(realcontent)
                         print hf[2]
                         print hf[3]
 
